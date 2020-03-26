d43535352ef4681b2c8342e869a91abb05518434
unknown <hdfclocal@hdfc-m6.hdfc.net>
2020-03-26 16:17:20 +0500

    first commit

---
+++ js/script.js
@@ -0,0 +1,140 @@
+$(() => {
+    var tasbeeha_1 = 0;
+    var tasbeeha_2 = 0;
+    var tasbeeha_3 = 0;
+    var tasbeeha_4 = 0;
+    var tasbeeha_5 = 0;
+    var tasbeeha_6 = 0;
+    var tasbeeha_7 = 0;
+    var tasbeeha_8 = 0;
+    var tasbeeha_9 = 0;
+    var tasbeeha_10 = 0;
+    var tasbeeha_11 = 0;
+
+    var tasbeeha_12 = 0;
+    var tasbeeha_13 = 0;
+    var tasbeeha_14 = 0;
+    var tasbeeha_15 = 0;
+    var tasbeeha_16 = 0;
+    var tasbeeha_17 = 0;
+    var tasbeeha_18 = 0;
+    var tasbeeha_19 = 0;
+    var tasbeeha_20 = 0;
+    var tasbeeha_21 = 0;
+    var tasbeeha_22 = 0;
+
+    $("#btn1").on("click", () => {
+        tasbeeha_1++;
+        $("#span_1").text(tasbeeha_1);
+    })
+    $("#btn2").on("click", () => {
+        tasbeeha_2++;
+        $("#span_2").text(tasbeeha_2);
+    })
+    $("#btn3").on("click", () => {
+        tasbeeha_3++;
+        $("#span_3").text(tasbeeha_3);
+    })
+    $("#btn4").on("click", () => {
+        tasbeeha_4++;
+        $("#span_4").text(tasbeeha_4);
+    })
+    $("#btn5").on("click", () => {
+        tasbeeha_5++;
+        $("#span_5").text(tasbeeha_5);
+    })
+    $("#btn6").on("click", () => {
+        tasbeeha_6++;
+        $("#span_6").text(tasbeeha_6);
+    })
+    $("#btn7").on("click", () => {
+        tasbeeha_7++;
+        $("#span_7").text(tasbeeha_7);
+    })
+    $("#btn8").on("click", () => {
+        tasbeeha_8++;
+        $("#span_8").text(tasbeeha_8);
+    })
+    $("#btn9").on("click", () => {
+        tasbeeha_9++;
+        $("#span_9").text(tasbeeha_9);
+    })
+    $("#btn10").on("click", () => {
+        tasbeeha_10++;
+        $("#span_10").text(tasbeeha_10);
+    })
+    $("#btn11").on("click", () => {
+        tasbeeha_11++;
+        $("#span_11").text(tasbeeha_11);
+    })
+
+
+    $("#btn12").on("click", () => {
+        tasbeeha_12++;
+        $("#span_12").text(tasbeeha_12);
+    })
+    $("#btn13").on("click", () => {
+        tasbeeha_13++;
+        $("#span_13").text(tasbeeha_13);
+    })
+    $("#btn14").on("click", () => {
+        tasbeeha_14++;
+        $("#span_14").text(tasbeeha_14);
+    })
+    $("#btn15").on("click", () => {
+        tasbeeha_15++;
+        $("#span_15").text(tasbeeha_15);
+    })
+    $("#btn16").on("click", () => {
+        tasbeeha_16++;
+        $("#span_16").text(tasbeeha_16);
+    })
+    $("#btn17").on("click", () => {
+        tasbeeha_17++;
+        $("#span_17").text(tasbeeha_17);
+    })
+    $("#btn17").on("click", () => {
+        tasbeeha_17++;
+        $("#span_17").text(tasbeeha_17);
+    })
+    $("#btn17").on("click", () => {
+        tasbeeha_17++;
+        $("#span_17").text(tasbeeha_17);
+    })
+    $("#btn17").on("click", () => {
+        tasbeeha_17++;
+        $("#span_17").text(tasbeeha_17);
+    })
+    $("#btn18").on("click", () => {
+        tasbeeha_18++;
+        $("#span_18").text(tasbeeha_18);
+    })
+    $("#btn19").on("click", () => {
+        tasbeeha_19++;
+        $("#span_19").text(tasbeeha_19);
+    })
+    $("#btn20").on("click", () => {
+        tasbeeha_20++;
+        $("#span_20").text(tasbeeha_20);
+    })
+    $("#btn21").on("click", () => {
+        tasbeeha_21++;
+        $("#span_21").text(tasbeeha_21);
+    })
+    $("#btn22").on("click", () => {
+        tasbeeha_22++;
+        $("#span_22").text(tasbeeha_22);
+    })
+    $("#morningImg").on("click", () => {
+        $("#morningDua").removeClass("hide");
+        $("#menu").addClass("hide");
+        $("#EveningDua").addClass("hide");
+      
+    })
+    $("#EveingImg").on("click", () => {
+        $("#morningDua").addClass("hide");
+        $("#menu").addClass("hide");
+        $("#EveningDua").removeClass("hide");
+       
+    })
+})
